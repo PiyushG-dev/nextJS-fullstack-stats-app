12e497d0383a888f745fb38aa9ed23b6a61acdb8
PiyushG-dev <pjgpiyush@gmail.com>
2024-03-11 18:36:49 +0530

    hero section ui complete

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "@/components/Hero/Hero";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import Image from "next/image";
+import CustomButton from "../CustomButton/CustomButton";
+import { heroText } from "@/constants";
+import figure1 from "@/public/figure1.png";
+import figure2 from "@/public/figure2.png";
+import dash from "@/public/dash.png";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={styles.wrapper}>
+      <Image src={figure1} alt="img" className={styles.figure_one} />
+      <div className={styles.heading_container}>
+        <h1>{heroText.heading}</h1>
+        <div className={styles.info_container}>
+          <p>{heroText.info}</p>
+          <CustomButton text="Get Started" />
+        </div>
+      </div>
+      <Image src={dash} alt="dashboard" className={styles.dash} />
+      <Image src={figure2} alt="img" className={styles.figure_two} />
+    </section>
+  );
 };
 
 export default Hero;
