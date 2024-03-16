27aa210cfe9f989c11e8f344a9073a6ae0bf3238
PiyushG-dev <pjgpiyush@gmail.com>
2024-03-16 23:02:56 +0530

    light and dark mode added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +12,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,27 +2,33 @@ import React from "react";
 import Link from "next/link";
 import { navLinks } from "@/constants";
 import { Button } from "./ui/button";
+import { ModeToggle } from "./ui/toggle";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center border-b-[1px] py-5 px-8 2xl:px-0 2xl:border-b-0">
-      <div className="flex items-center gap-6">
-        <Link href="/" className="text-base">
-          Venture studio
-        </Link>
-        <div className="flex gap-6">
-          {navLinks.map((link) => (
-            <Link
-              key={link.id}
-              href={link.url}
-              className="text-muted-foreground font-light text-sm 2xl:text-base"
-            >
-              {link.title}
-            </Link>
-          ))}
+    <div className="border-b-[1px] py-5 px-8 2xl:px-0 2xl:flex 2xl:justify-center">
+      <div className="flex justify-between items-center 2xl:w-[1536px]">
+        <div className="flex items-center gap-6">
+          <Link href="/" className="text-base">
+            Venture studio
+          </Link>
+          <div className="flex gap-6">
+            {navLinks.map((link) => (
+              <Link
+                key={link.id}
+                href={link.url}
+                className="text-muted-foreground font-light text-sm 2xl:text-base"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-6">
+          <Button>Get started</Button>
+          <ModeToggle />
         </div>
       </div>
-      <Button>Get started</Button>
     </div>
   );
 };
