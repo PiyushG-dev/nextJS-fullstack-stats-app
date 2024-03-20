b17cd40cc7c072d5d53af62af63cf5bd2a2b183c
Piyush Goswami <pjgpiyush@gmail.com>
2024-03-20 22:41:14 +0530

    bug fixes

--- utils/index.js
+++ utils/index.js
@@ -2,10 +2,7 @@ const url =
   "https://football-web-pages1.p.rapidapi.com/league-table.json?comp=1&team=1";
 
 export const fetchLeagues = async () => {
-  const headers = {
-    "X-RapidAPI-Key": "53ca4eced4msh88e3bff0c957532p10fd39jsn05892f6d865a",
-    "X-RapidAPI-Host": "football-web-pages1.p.rapidapi.com",
-  };
+  const headers = {};
 
   const response = await fetch(url, {
     headers: headers,
